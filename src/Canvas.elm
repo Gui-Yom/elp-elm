555e1d104d1a9ba6e1db0f6704fd027adc82d76a
Guillaume Anthouard <guillaume.anthouard@hotmail.fr>
2021-12-26 18:01:07 +0100

    More styling, split in components

---
+++ src/Canvas.elm
@@ -0,0 +1,89 @@
+module Canvas exposing (..)
+
+import Dict
+import Html exposing (Html)
+import Program exposing (Inst(..), Proc, Program)
+import Svg exposing (Svg, line, svg)
+import Svg.Attributes exposing (height, id, style, viewBox, width, x1, x2, y1, y2)
+
+
+
+-- VIEW
+
+
+type alias Cursor =
+    { x : Float, y : Float, angle : Float }
+
+
+correctAngle : Float -> Float
+correctAngle angle =
+    if angle >= 360 then
+        angle - 360
+
+    else if angle < 0 then
+        angle + 360
+
+    else
+        angle
+
+
+drawProgram : Program -> Cursor -> List (Svg msg)
+drawProgram prog cursor =
+    drawProc prog prog.main cursor
+
+
+drawProc : Program -> Proc -> Cursor -> List (Svg msg)
+drawProc prog proc cursor =
+    case proc of
+        [] ->
+            []
+
+        inst :: subProc ->
+            case inst of
+                Forward length ->
+                    let
+                        newCursor =
+                            { cursor
+                                | x = cursor.x + length * cos (degrees cursor.angle)
+                                , y = cursor.y + length * sin (degrees cursor.angle)
+                            }
+                    in
+                    [ line
+                        [ x1 (String.fromFloat cursor.x)
+                        , y1 (String.fromFloat cursor.y)
+                        , x2 (String.fromFloat newCursor.x)
+                        , y2 (String.fromFloat newCursor.y)
+                        , style "stroke:rgb(255,0,0);stroke-width:2"
+                        ]
+                        []
+                    ]
+                        ++ drawProc prog subProc newCursor
+
+                Left n ->
+                    drawProc prog subProc { cursor | angle = correctAngle (cursor.angle - n) }
+
+                Right n ->
+                    drawProc prog subProc { cursor | angle = correctAngle (cursor.angle + n) }
+
+                Repeat n toRepeat ->
+                    if n <= 1 then
+                        drawProc prog (toRepeat ++ subProc) cursor
+
+                    else
+                        drawProc prog (toRepeat ++ [ Repeat (n - 1) toRepeat ] ++ subProc) cursor
+
+                Call procName ->
+                    drawProc prog (Maybe.withDefault [] (Dict.get procName prog.procs) ++ subProc) cursor
+
+
+view : Maybe Program -> Html msg
+view mprog =
+    svg
+        [ id "canvas", width "500", height "500", viewBox "0 0 500 500" ]
+        (case mprog of
+            Just prog ->
+                drawProgram prog { x = 250, y = 250, angle = 0 }
+
+            Nothing ->
+                []
+        )
--- src/Main.elm
+++ src/Main.elm
@@ -1,15 +1,15 @@
 module Main exposing (main)
 
 import Browser
-import Dict
-import Html exposing (Html, div, li, p, text, textarea, ul)
+import Canvas
+import Html exposing (Html, div, p, text, textarea)
 import Html.Attributes exposing (autofocus, class, cols, id, placeholder, rows, value)
 import Html.Events exposing (onInput)
+import Html.Lazy exposing (lazy, lazy2)
 import LocalStorage exposing (saveProgText)
 import Parser exposing (DeadEnd)
 import Program exposing (Inst(..), Proc, Program, parseProgram)
-import Svg exposing (Svg, line, svg)
-import Svg.Attributes exposing (height, style, viewBox, width, x1, x2, y1, y2)
+import ProgramList
 
 
 
@@ -25,12 +25,13 @@ main =
 
 
 type alias Model =
-    { progText : String, lastSuccessful : Maybe Program, error : Maybe (List DeadEnd) }
+    { progText : String, lastSuccessful : Maybe Program, error : Maybe (List DeadEnd), plModel : ProgramList.Model }
 
 
 init : Maybe String -> ( Model, Cmd Msg )
 init flags =
-    ( { progText = Maybe.withDefault "" flags, lastSuccessful = Nothing, error = Nothing }, Cmd.none )
+    update (ProgramTextUpdated (Maybe.withDefault "" flags))
+        { progText = "", lastSuccessful = Nothing, error = Nothing, plModel = ProgramList.init }
 
 
 
@@ -39,11 +40,15 @@ init flags =
 
 type Msg
     = ProgramTextUpdated String
+    | ProgramListMsg ProgramList.Msg
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        ProgramListMsg plmsg ->
+            ( { model | plModel = ProgramList.update plmsg model.plModel }, Cmd.none )
+
         ProgramTextUpdated newProgText ->
             let
                 result =
@@ -71,112 +76,6 @@ update msg model =
 
 
 
--- VIEW (Show Program)
-
-
-showProgram : Program -> List (Html Msg)
-showProgram prog =
-    [ ul []
-        (List.map
-            (\tuple ->
-                li [] [ text (Tuple.first tuple), showProc (Tuple.second tuple) ]
-            )
-            (Dict.toList prog.procs)
-        )
-    , showProc prog.main
-    ]
-
-
-showProc : Proc -> Html Msg
-showProc proc =
-    ul []
-        (List.map
-            (\inst ->
-                case inst of
-                    Repeat n subProg ->
-                        li [] [ text ("Repeat " ++ String.fromInt n), showProc subProg ]
-
-                    other ->
-                        li [] [ text (Debug.toString other) ]
-            )
-            proc
-        )
-
-
-
--- VIEW (Draw Program)
-
-
-type alias Cursor =
-    { x : Float, y : Float, angle : Float }
-
-
-drawProgram : Program -> Cursor -> List (Svg msg)
-drawProgram prog cursor =
-    drawProc prog prog.main cursor
-
-
-drawProc : Program -> Proc -> Cursor -> List (Svg msg)
-drawProc prog proc cursor =
-    case proc of
-        [] ->
-            []
-
-        inst :: subProc ->
-            case inst of
-                Forward length ->
-                    let
-                        newCursor =
-                            { cursor
-                                | x = cursor.x + length * cos (degrees cursor.angle)
-                                , y = cursor.y + length * sin (degrees cursor.angle)
-                            }
-                    in
-                    [ line
-                        [ x1 (String.fromFloat cursor.x)
-                        , y1 (String.fromFloat cursor.y)
-                        , x2 (String.fromFloat newCursor.x)
-                        , y2 (String.fromFloat newCursor.y)
-                        , style "stroke:rgb(255,0,0);stroke-width:2"
-                        ]
-                        []
-                    ]
-                        ++ drawProc prog subProc newCursor
-
-                Left n ->
-                    drawProc prog subProc { cursor | angle = correctAngle (cursor.angle - n) }
-
-                Right n ->
-                    drawProc prog subProc { cursor | angle = correctAngle (cursor.angle + n) }
-
-                Repeat n toRepeat ->
-                    if n <= 1 then
-                        drawProc prog (toRepeat ++ subProc) cursor
-
-                    else
-                        drawProc prog (toRepeat ++ [ Repeat (n - 1) toRepeat ] ++ subProc) cursor
-
-                Call procName ->
-                    drawProc prog (Maybe.withDefault [] (Dict.get procName prog.procs) ++ subProc) cursor
-
-
-
--- TODO proc call
-
-
-correctAngle : Float -> Float
-correctAngle angle =
-    if angle >= 360 then
-        angle - 360
-
-    else if angle < 0 then
-        angle + 360
-
-    else
-        angle
-
-
-
 -- MAIN VIEW
 
 
@@ -201,20 +100,10 @@ view model =
                 Nothing ->
                     case model.lastSuccessful of
                         Just prog ->
-                            p [] (showProgram prog)
+                            Html.map (\plmsg -> ProgramListMsg plmsg) (lazy2 ProgramList.view model.plModel prog)
 
                         Nothing ->
                             p [ id "errorMsg" ] [ text "Please enter a program" ]
             ]
-        , div [ class "column" ]
-            [ svg
-                [ id "canvas", width "500", height "500", viewBox "0 0 500 500" ]
-                (case model.lastSuccessful of
-                    Just prog ->
-                        drawProgram prog { x = 250, y = 250, angle = 0 }
-
-                    Nothing ->
-                        []
-                )
-            ]
+        , div [ class "column" ] [ lazy Canvas.view model.lastSuccessful ]
         ]
